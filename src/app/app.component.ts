f68ff8029cb57c44dcd73f2df72ad780ab36fa08
pranavlawrence <pranavlawrence40@gmail.com>
2019-01-18 00:39:49 +0530

    modified html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
-import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  template: 
-  
-  `Enter Movie name: <input type="text" (keyup.enter)="getMovieData()" [{ngModel}]="title"/>`,
-  
-  
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
   title;
 
-  getMovieData(){
+  Data(){
+    
 
 
   }
--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -8,5 +8,7 @@ export class OmdbService {
 
   constructor(private http:HttpClient) { }
 
-  
+  movieData(title){
+    return this.http.get("http://www.omdbapi.com/?t="+title+"&apikey=c2f138d6")
+  }
 }
